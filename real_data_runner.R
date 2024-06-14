d472b7298837be215b57961e8947f9f6599c8956
Eamon Conway <conway.e@wehi.edu.au>
2024-06-14 17:45:11 +1000

    Changed filenames

--- real_data_runner.R
+++ real_data_runner.R
@@ -20,19 +20,19 @@ for(window_size in list(1,2,4,7, "Random")){
   windows <- windows[which(windows < 100)]
   for(filter_size in c(1,2)){
 
-    
+
     # Real Data
-    real_data <- read_csv("victoria_data.csv") %>% 
-      select(diagnosis_date,Total_case_count) %>% 
-      group_by(diagnosis_date) %>% 
+    real_data <- read_csv("victoria_data.csv") %>%
+      select(diagnosis_date,Total_case_count) %>%
+      group_by(diagnosis_date) %>%
       summarise(cases = sum(Total_case_count)) %>%
-      mutate(diagnosis_date = ymd(diagnosis_date)) %>% 
+      mutate(diagnosis_date = ymd(diagnosis_date)) %>%
       filter(diagnosis_date < dmy("01-09-2021") + 7*15,
              diagnosis_date >= dmy("01-09-2021")) %>%
       mutate(date_group = cut(diagnosis_date,
                               breaks = min(diagnosis_date) + windows) %>%
-               as_date()) 
-    
+               as_date())
+
     aggregated_data_full <- real_data %>%
       group_by(date_group) %>%
       summarise(WeeklyIncidence = sum(cases),window = n()) %>%
@@ -42,24 +42,24 @@ for(window_size in list(1,2,4,7, "Random")){
              day = day - min(as.integer(date_group)))
 
     rows_to_keep <- sample(1:nrow(real_data),floor(nrow(real_data)/filter_size))
-    
-    aggregated_data <- aggregated_data_full %>% 
+
+    aggregated_data <- aggregated_data_full %>%
       filter(row_number() %in% rows_to_keep)
-    
-    missing_data <- aggregated_data_full %>% 
+
+    missing_data <- aggregated_data_full %>%
       filter(!(row_number() %in% rows_to_keep))
-    
+
     xmax <- missing_data$week-1
     xmin <- xmax - missing_data$window
-    
-    daily_data <- real_data %>% 
-      filter(diagnosis_date < max(aggregated_data$week))
-    
 
-    
-    daily_data <- real_data %>% 
+    daily_data <- real_data %>%
       filter(diagnosis_date < max(aggregated_data$week))
-    
+
+
+
+    daily_data <- real_data %>%
+      filter(diagnosis_date < max(aggregated_data$week))
+
     stan_data <- list(
       N_data = nrow(aggregated_data),
       D = aggregated_data$WeeklyIncidence,
@@ -74,66 +74,66 @@ for(window_size in list(1,2,4,7, "Random")){
       beta = beta,
       lambda = lambda
     )
-    
-    fit <- rstan::stan(file = "binomial_aggregate.stan",
-                       data = stan_data, 
+
+    fit <- rstan::stan(file = "reduced_data.stan",
+                       data = stan_data,
                        warmup = 2000,
-                       iter = 8000, 
+                       iter = 8000,
                        chains = 4,
                        cores = 4,
-                       control = list(max_treedepth = 12)) 
-    
+                       control = list(max_treedepth = 12))
+
     summary_fit <- summarise_model_fit(fit,
                                        c(0.025,0.25,0.75,0.975))
-    
-    Iforecast_plot_data = summary_fit$I_forecast %>% 
+
+    Iforecast_plot_data = summary_fit$I_forecast %>%
       format_plot_data(.,max(aggregated_data$week))
-    
+
     Rforecast_plot_data = summary_fit$R_forecast %>%
       format_plot_data(.,max(aggregated_data$week))
-    
+
     R_plot_data <- summary_fit$R_T %>%
       format_plot_data(.,min(real_data$diagnosis_date))
-    
+
     I_plot_data <- summary_fit$I_T %>%
       format_plot_data(.,min(real_data$diagnosis_date))
-    
-    
-    figure1 <- ggplot(daily_data, aes(x = diagnosis_date)) + 
+
+
+    figure1 <- ggplot(daily_data, aes(x = diagnosis_date)) +
       geom_ribbon(data = I_plot_data, aes(ymin = mid_lower,ymax = mid_upper),alpha = 0.9,fill = "#7570b3") +
-      geom_ribbon(data = I_plot_data, aes(ymin = lower,ymax = upper),alpha = 0.5,fill = "#7570b3") + 
-      geom_vline(xintercept = aggregated_data$week-1,linetype="dotted") + 
+      geom_ribbon(data = I_plot_data, aes(ymin = lower,ymax = upper),alpha = 0.5,fill = "#7570b3") +
+      geom_vline(xintercept = aggregated_data$week-1,linetype="dotted") +
       geom_point(aes(y = cases),size = 0.6)  +
       scale_y_continuous("Daily Incidence")+
       scale_x_date("Day") +
       theme_classic() +
-      theme(text = element_text(size=16)) 
+      theme(text = element_text(size=16))
     if(length(xmax)!=0){
       for(i in 1:length(xmax)){
         figure1 <- figure1 + annotate("rect",xmin = xmin[i],xmax=xmax[i],ymin = -Inf,ymax = Inf,alpha = 0.2)
       }
     }
     ggsave(paste0("final_pictures/RealData/IncidenceWindow", window_size, "Filter",filter_size,"Area", Area, ".svg"),plot = figure1,, width = 89,height = 60,units="mm",dpi=300,create.dir = TRUE)
-    
-    figure2 <- ggplot(R_plot_data,aes(x = diagnosis_date)) + 
+
+    figure2 <- ggplot(R_plot_data,aes(x = diagnosis_date)) +
       geom_ribbon( aes(ymin = mid_lower,ymax = mid_upper),alpha = 0.8,fill = "#7570b3") +
-      geom_ribbon(data = R_plot_data, aes(ymin = lower,ymax = upper),alpha = 0.5,fill = "#7570b3") + 
-      geom_vline(xintercept = aggregated_data$week-1,linetype="dotted") + 
+      geom_ribbon(data = R_plot_data, aes(ymin = lower,ymax = upper),alpha = 0.5,fill = "#7570b3") +
+      geom_vline(xintercept = aggregated_data$week-1,linetype="dotted") +
       scale_y_continuous("R Effective")+
       coord_cartesian(ylim = c(0,2.5))+
       scale_x_date("Day") +
       theme_classic() +
-      theme(text = element_text(size=16)) 
+      theme(text = element_text(size=16))
     if(length(xmax)!=0){
       for(i in 1:length(xmax)){
         figure2 <- figure2 + annotate("rect",xmin = xmin[i],xmax=xmax[i],ymin = -Inf,ymax = Inf,alpha = 0.2)
       }
     }
-    
+
     phi_plot <- mcmc_areas(fit,"phi",transformations = function(x) 1/x) + scale_x_continuous("Overdispersion")
     ggsave(paste0("final_pictures/RealData/PhiWindow", window_size, "Filter", filter_size,"Area", Area, ".svg"),plot = phi_plot, width = 89,height = 60,units="mm",dpi=300,create.dir = TRUE)
     ggsave(paste0("final_pictures/RealData/ReproductionWindow", window_size, "Filter", filter_size,"Area", Area, ".svg"),plot = figure2, width = 89,height = 60,units="mm",dpi=300,create.dir = TRUE)
-    
+
     daily_stan_data <- list(
       N_days = nrow(daily_data),
       D = daily_data$cases,
@@ -144,26 +144,26 @@ for(window_size in list(1,2,4,7, "Random")){
       alpha = alpha,
       beta = beta,
       lambda = lambda
-    ) 
-    
-    
+    )
+
+
     daily_fit <- stan(file = "daily.stan",
-                      data = daily_stan_data, 
+                      data = daily_stan_data,
                       warmup = 2000,
-                      iter = 8000, 
+                      iter = 8000,
                       chains = 4,
                       cores = 4)
     daily_summary <- summarise_daily_model_fit(daily_fit,
                                                c(0.025,0.25,0.75,0.975))
-    
-    
+
+
     R_plot_data_combined <- daily_summary$R_T %>%
       format_plot_data(.,min(daily_data$diagnosis_date)) %>% bind_rows(.,R_plot_data,.id = "Model")
-    
-     figure2_compare <- ggplot(R_plot_data_combined,aes(x = diagnosis_date)) + 
+
+     figure2_compare <- ggplot(R_plot_data_combined,aes(x = diagnosis_date)) +
       geom_ribbon( aes(ymin = mid_lower,ymax = mid_upper, fill = Model),alpha = 0.9) +
-      geom_ribbon(aes(ymin = lower,ymax = upper, fill = Model),alpha = 0.6) + 
-      geom_vline(xintercept = aggregated_data$week-1,linetype="dotted") + 
+      geom_ribbon(aes(ymin = lower,ymax = upper, fill = Model),alpha = 0.6) +
+      geom_vline(xintercept = aggregated_data$week-1,linetype="dotted") +
       scale_y_continuous("R Effective")+
        coord_cartesian(ylim = c(0,2.5))+
       scale_fill_manual(values = c("2"="#7570b3","1"="#d95f02"),
@@ -173,16 +173,16 @@ for(window_size in list(1,2,4,7, "Random")){
       theme(text = element_text(size=16),
             legend.title = element_blank(),
         legend.text = element_text(size = 10,),
-        legend.position = "inside", 
+        legend.position = "inside",
         legend.justification = c(0.90,0.95),
       )
-     
+
      if(length(xmax)!=0){
        for(i in 1:length(xmax)){
          figure2_compare <- figure2_compare + annotate("rect",xmin = xmin[i],xmax=xmax[i],ymin = -Inf,ymax = Inf,alpha = 0.2)
        }
      }
-     
+
     ggsave(paste0("final_pictures/RealData/ReproductionWindow", window_size, "Filter", filter_size, "Area", Area, "Compare.svg"),plot = figure2_compare, width = 89,height = 60,units="mm",dpi=300,create.dir = TRUE)
     phi_plot <- mcmc_areas(daily_fit,"phi",transformations = function(x) 1/x) + scale_x_continuous("Overdispersion")
     ggsave(paste0("final_pictures/RealData/DailyPhiWindow", window_size, "Filter", filter_size,"Area", Area, ".svg"),plot = phi_plot, width = 89,height = 60,units="mm",dpi=300,create.dir = TRUE)
--- simulated_data_runner.R
+++ simulated_data_runner.R
@@ -9,15 +9,15 @@ R_t <- rep(1.5,n_days)
 R_t[40:n_days] <- 0.8
 R_t <- 0.8*cos((1:n_days)*2*pi/n_days) + 1.2
 simulated_data <- tibble(diagnosis_date = 1:n_days,
-                         R_t = R_t, 
-                         Total_case_count = generate_simulated_incidence(R_T,g,100)) 
+                         R_t = R_t,
+                         Total_case_count = generate_simulated_incidence(R_T,g,100))
 Area = 50
 alpha = 1e-3
 beta = 1e-3
 lambda = -log(Area/100)
 
 for(window_size in list(1,2,4,7,"Random")){
-  
+
   if(is.numeric(window_size)){
     windows <- seq(0,100,by=window_size)
   } else {
@@ -26,13 +26,13 @@ for(window_size in list(1,2,4,7,"Random")){
   windows <- windows[which(windows < 100)]
 
   # Data
-  data <- simulated_data %>% 
+  data <- simulated_data %>%
     select(diagnosis_date,Total_case_count) %>%
-    group_by(diagnosis_date) %>% 
+    group_by(diagnosis_date) %>%
     summarise(cases = sum(Total_case_count)) %>%
     mutate(date_group = cut(diagnosis_date,
                             breaks = 0 + windows,include.lowest = TRUE))
-  
+
   for(filter_size in c(1,2)){
     aggregated_data_full <- data %>%
       group_by(date_group) %>%
@@ -42,19 +42,19 @@ for(window_size in list(1,2,4,7,"Random")){
              day = cumsum(window),
              week = (day))
     rows_to_keep <- sample(1:nrow(data),floor(nrow(data)/filter_size))
-    
-    aggregated_data <- aggregated_data_full %>% 
+
+    aggregated_data <- aggregated_data_full %>%
       filter(row_number() %in% rows_to_keep)
-    
-    missing_data <- aggregated_data_full %>% 
+
+    missing_data <- aggregated_data_full %>%
       filter(!(row_number() %in% rows_to_keep))
-    
+
     xmax <- missing_data$day
     xmin <- xmax - missing_data$window
-    
-    daily_data <- simulated_data %>% 
+
+    daily_data <- simulated_data %>%
       filter(diagnosis_date < max(aggregated_data$week))
-    
+
     IncidenceInitialCondition <- rep(0,length(g))
     IncidenceInitialCondition[1] <- 100
     stan_data <- list(
@@ -71,64 +71,64 @@ for(window_size in list(1,2,4,7,"Random")){
       beta = beta,
       lambda = lambda
     )
-    
-    fit <- rstan::stan(file = "binomial_aggregate_known.stan",
-                       data = stan_data, 
-                       iter = 10000, 
+
+    fit <- rstan::stan(file = "reduced_data_known_init.stan",
+                       data = stan_data,
+                       iter = 10000,
                        chains = 4,
                        cores = 4,
-                       control = list(max_treedepth = 12)) 
-    
+                       control = list(max_treedepth = 12))
+
     summary_fit <- summarise_model_fit(fit,
                                        c(0.025,0.25,0.75,0.975))
-    
-    Iforecast_plot_data = summary_fit$I_forecast %>% 
+
+    Iforecast_plot_data = summary_fit$I_forecast %>%
       format_plot_data(.,max(aggregated_data$week))
-    
+
     Rforecast_plot_data = summary_fit$R_forecast %>%
       format_plot_data(.,max(aggregated_data$week))
-    
+
     R_plot_data <- summary_fit$R_T %>%
       format_plot_data(.,min(data$diagnosis_date))
-    
+
     I_plot_data <- summary_fit$I_T %>%
       format_plot_data(.,min(data$diagnosis_date))
-    
-    
-    figure1 <- ggplot(daily_data, aes(x = diagnosis_date)) + 
+
+
+    figure1 <- ggplot(daily_data, aes(x = diagnosis_date)) +
       geom_ribbon(data = I_plot_data, aes(ymin = mid_lower,ymax = mid_upper),alpha = 0.8,fill = "#7570b3") +
-      geom_ribbon(data = I_plot_data, aes(ymin = lower,ymax = upper),alpha = 0.5,fill = "#7570b3") + 
-      geom_vline(xintercept = aggregated_data$week,linetype="dotted") + 
+      geom_ribbon(data = I_plot_data, aes(ymin = lower,ymax = upper),alpha = 0.5,fill = "#7570b3") +
+      geom_vline(xintercept = aggregated_data$week,linetype="dotted") +
       geom_point(aes(y = Total_case_count),size = 0.5)  +
       scale_y_continuous("Daily Incidence")+
       scale_x_continuous("Day") +
       theme_classic() +
-      theme(text = element_text(size=16)) 
-    
+      theme(text = element_text(size=16))
+
     if(length(xmax)!=0){
     for(i in 1:length(xmax)){
       figure1 <- figure1 + annotate("rect",xmin = xmin[i],xmax=xmax[i],ymin = -Inf,ymax = Inf,alpha = 0.2)
     }
     }
     ggsave(paste0("final_pictures/SimulatedData/Cos/IncidenceWindow", window_size, "Filter",filter_size,"Area", Area, ".svg"),plot = figure1, width = 89,height = 60,units="mm",dpi=300,create.dir = TRUE)
-    
-    figure2 <- ggplot(R_plot_data,aes(x = diagnosis_date)) + 
+
+    figure2 <- ggplot(R_plot_data,aes(x = diagnosis_date)) +
       geom_ribbon( aes(ymin = mid_lower,ymax = mid_upper),alpha = 0.8,fill = "#7570b3") +
-      geom_ribbon(data = R_plot_data, aes(ymin = lower,ymax = upper),alpha = 0.5,fill = "#7570b3") + 
-      geom_vline(xintercept = aggregated_data$week,linetype="dotted") + 
+      geom_ribbon(data = R_plot_data, aes(ymin = lower,ymax = upper),alpha = 0.5,fill = "#7570b3") +
+      geom_vline(xintercept = aggregated_data$week,linetype="dotted") +
       geom_point(data = daily_data,aes(y = R_t),size = 0.5)  +
-      scale_y_continuous("R Effective",limits = c(0,NULL))+ 
+      scale_y_continuous("R Effective",limits = c(0,NULL))+
       coord_cartesian(ylim = c(0,3))+
       scale_x_continuous("Day") +
       theme_classic() +
-      theme(text = element_text(size=16)) 
-    
+      theme(text = element_text(size=16))
+
     if(length(xmax)!=0){
       for(i in 1:length(xmax)){
         figure2 <- figure2 + annotate("rect",xmin = xmin[i],xmax=xmax[i],ymin = -Inf,ymax = Inf,alpha = 0.2)
       }
     }
-    
+
     phi_plot <- mcmc_areas(fit,"phi",transformations = function(x) 1/x) + scale_x_continuous("Overdispersion")
     ggsave(paste0("final_pictures/SimulatedData/Cos/PhiWindow", window_size, "Filter", filter_size,"Area", Area, ".svg"),plot = phi_plot, width = 89,height = 60,units="mm",dpi=300,create.dir = TRUE)
     ggsave(paste0("final_pictures/SimulatedData/Cos/ReproductionWindow", window_size, "Filter", filter_size,"Area", Area, ".svg"),plot = figure2, width = 89,height = 60,units="mm",dpi=300,create.dir = TRUE)
