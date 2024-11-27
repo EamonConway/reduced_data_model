ea255c70c97d79540e5126123ece092dbc11d3a6
Eamon Conway <conway.e@wehi.edu.au>
2024-11-27 12:08:54 +1100

    Remove library headers. Add map for scenario analysis

--- _targets.R
+++ _targets.R
@@ -1,9 +1,6 @@
 # Load packages required to define the pipeline:
 library(targets)
-library(readr)
-library(magrittr)
-library(dplyr)
-library(lubridate)
+library(tarchetypes)
 # Set target options:
 tar_option_set(
   packages = c("tidyr","tibble","readr","magrittr","dplyr","lubridate","rstan","bayesplot","ggplot2","StanHeaders") # Packages that your targets need for their tasks.
@@ -11,10 +8,15 @@ tar_option_set(
 
 # Run the R scripts in the R/ folder with your custom functions:
 tar_source()
+scenarios  = tibble(area = 30,window = 7,filter = 1)
 
 # List of targets for paper.
 list(
   tar_target(data_file, "data/victoria_data.csv", format = "file"),
+  tar_target(real_data, load_and_filter_real_data(data_file)),
   tar_target(covid_generation_interval,define_generation_interval(3.6,3.1,16)),
-  tar_target(plot, reduced_data_model(covid_generation_interval, 30,7,1))
+  # tar_target(daily_fit,daily_model(real_data,covid_generation_interval)),
+  tar_map(values = scenarios,
+  tar_target(covid_fit, reduced_data_model(real_data, covid_generation_interval, area,window,filter))
+  )
 )
