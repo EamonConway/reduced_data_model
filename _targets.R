eaf293e2398cbb21dc825e359e334a72367a9b49
Eamon Conway <conway.e@wehi.edu.au>
2024-11-27 14:39:24 +1100

    Include alpha and beta in targets and nest map
    We need to iterate over area, window and filter size but we do not wish to double up on daily models.
    Area is mapped first, then we map the other parameters in our scenarios.

--- _targets.R
+++ _targets.R
@@ -3,20 +3,52 @@ library(targets)
 library(tarchetypes)
 # Set target options:
 tar_option_set(
-  packages = c("tidyr","tibble","readr","magrittr","dplyr","lubridate","rstan","bayesplot","ggplot2","StanHeaders") # Packages that your targets need for their tasks.
+  packages = c(
+    "tidyr",
+    "tibble",
+    "readr",
+    "magrittr",
+    "dplyr",
+    "lubridate",
+    "rstan",
+    "bayesplot",
+    "ggplot2",
+    "StanHeaders"
+  ) # Packages that your targets need for their tasks.
 )
 
 # Run the R scripts in the R/ folder with your custom functions:
 tar_source()
-scenarios  = tibble(area = 30,window = 7,filter = 1)
+alpha <- 1e-2
+beta <- 1e-2
+area_scenarios = tibble(area = 30)
+missing_scenarios = tibble(window = 7, filter = 1)
 
 # List of targets for paper.
 list(
   tar_target(data_file, "data/victoria_data.csv", format = "file"),
   tar_target(real_data, load_and_filter_real_data(data_file)),
-  tar_target(covid_generation_interval,define_generation_interval(3.6,3.1,16)),
-  # tar_target(daily_fit,daily_model(real_data,covid_generation_interval)),
-  tar_map(values = scenarios,
-  tar_target(covid_fit, reduced_data_model(real_data, covid_generation_interval, area,window,filter))
+  tar_target(
+    covid_generation_interval,
+    define_generation_interval(3.6, 3.1, 16)
+  ),
+  tar_map(
+    values = area_scenarios,
+    tar_target(
+      daily_fit,
+      daily_model(real_data, covid_generation_interval, alpha, beta, area)
+    ),
+    tar_map(values = missing_scenarios, tar_target(
+      covid_fit,
+      reduced_data_model(
+        real_data,
+        covid_generation_interval,
+        area,
+        window,
+        filter,
+        alpha,
+        beta
+      )
+    ))
   )
 )
